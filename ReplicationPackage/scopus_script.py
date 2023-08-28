ad4cd305520ffe6f694684a3adc11c1dff4c8752
jamacedo <jamacedo@criticalsoftware.com>
2023-08-28 19:06:57 +0100

    retrieving more results and add them to a csv

--- ReplicationPackage/scopus_script.py
+++ ReplicationPackage/scopus_script.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 # TODO: Rate Limits and Pagination
 # The Scopus API has rate limits. 
@@ -6,7 +7,7 @@ import requests
 # The script below fetches a set number of results (default 25). 
 # If the search yields more results, there is a need to implement pagination to retrieve all.
 
-def search_scopus(query, api_key, count=25):
+def search_scopus(query, api_key,start=0, count=25):
     base_url = "https://api.elsevier.com/content/search/scopus"
     
     headers = {
@@ -17,14 +18,45 @@ def search_scopus(query, api_key, count=25):
     params = {
         "query": query,
         "count": count,
+        "start": start
     }
     
     response = requests.get(base_url, headers=headers, params=params)
     
     if response.status_code == 200:
-        return response.json()
+        data = response.json()
+        return data['search-results'].get('entry', [])  # Return empty list if 'entry' doesn't exist
     else:
         response.raise_for_status()
+
+def extract_information(data):
+    extracted = []
+    
+    for item in data['search-results']['entry']:
+        title = item['dc:title']
+        
+        # Correctly extract the publication year
+        cover_date = item.get('prism:coverDate')
+        if cover_date and isinstance(cover_date, str):
+            publication_year = cover_date.split('-')[0]
+        else:
+            publication_year = None
+        
+        journal = item['prism:publicationName']
+        
+        authors = []
+        if 'author' in item:
+            for author in item['author']:
+                authors.append(author['authname'])
+        
+        extracted.append({
+            'Title': title,
+            'Publication Year': publication_year,
+            'Journal': journal,
+            'Authors': ', '.join(authors)
+        })
+    
+    return extracted
 
 
 if __name__ == "__main__":
@@ -33,16 +65,35 @@ if __name__ == "__main__":
 
     # TODO: create the query by collection the terms from a csv
     # query = "(agile OR agility OR xp OR ”extreme W/0 programming” OR scrum OR kanban OR scrumban OR safescrum OR agilesafe OR ”agile W/0 safe”) AND (safety OR ”safety W/0 systems” OR safetycritical OR ”safety W/0 critical” OR ”safety-critical W/0 systems” OR ”safety W/0 critical W/0 systems” OR ”high W/0 integrity” OR ”high W/0 integrity W/0 systems” OR his OR ”safety W/0 integrity”) AND (aerospace OR avionic OR avionics OR aviation OR aeronautic OR aeronautics OR aeronautical) OR (”ARP W/0 4761” OR arp4761 OR ”ARP W/0 4754” OR arp4754 OR do-178 OR do-178b OR DO 178C OR do178 OR do178b OR do178c OR do-331 OR do331)"  
-    query = "docops"
+    QUERY = "docops"
 
+    
     # TODO: Analyzing and Parsing the Results
     # Modify the script to extract more details as per the requirements.
     # The Scopus API returns a lot more information, such as authors, journal name, publication year, etc.
     # The script below prints only the titles of the articles returned by the search for now.
-    results = search_scopus(query, API_KEY)
-    for item in results['search-results']['entry']:
-        print(item['dc:title'])
+    # results = search_scopus(query, API_KEY)
+    # for item in results['search-results']['entry']:
+    #     print(item['dc:title'])
+    
+    all_results = []
+    start_index = 0
+    PAGE_SIZE = 25
+    TOTAL_RESULTS = 250  # Adjust if needed
+    
+    while start_index < TOTAL_RESULTS:
+        print(f"Fetching results starting from index {start_index}...")
+        entries = search_scopus(QUERY, API_KEY, start=start_index, count=PAGE_SIZE)
+        
+        if not entries:  # Break the loop if there are no more entries.
+            break
 
+        all_results.extend(extract_information({"search-results": {"entry": entries}}))
+        start_index += PAGE_SIZE
+        
+    df = pd.DataFrame(all_results)
+    df.to_csv("scopus_results.csv", index=False)
+    print("Results saved to scopus_results.csv")
 
 
 # TODO: Saving results to a CSV or database.
