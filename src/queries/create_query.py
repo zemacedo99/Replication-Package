73dffa7a5538976a433f511a50374e2593a38a23
jamacedo <up201705226@fe.up.pt>
2023-10-11 18:41:12 +0100

    fix create query

--- src/queries/create_query.py
+++ src/queries/create_query.py
@@ -1,17 +1,25 @@
 import pandas as pd
+import os
 
-def create_query_from_csv(csv_path):
+def create_query_from_csv(filename):
+
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    csv_path = os.path.join(script_directory, filename)
+    
     # Read the CSV
     df = pd.read_csv(csv_path)
+
+    # Ensure 'Term' is of string type
+    df['Term'] = df['Term'].astype(str)
     
     # Group by 'Category' and join terms with ' OR '
-    query_groups = df.groupby('Category')['Term'].apply(lambda x: f"({') OR ('.join(x)})").tolist()
+    query_groups = df.groupby('Category')['Term'].apply(lambda x: f"({' OR '.join(x)})").tolist()
     
     # Join groups with ' AND '
     query = ' AND '.join(query_groups)
-    
+
     return query
 
-# Assuming 'terms.csv' is your file
-query = create_query_from_csv('C:\Users\jamacedo\OneDrive - CRITICAL SOFTWARE, S.A\Documents\FEUP\Thesis\Git\terms.csv')
+# Assuming 'query_terms.csv' is your file
+query = create_query_from_csv("query_terms.csv")
 print(query)
